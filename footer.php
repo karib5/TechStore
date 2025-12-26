50c3b7e9d6162b0dc2441568045ef9d668756a12
karib5 <karibhasan5@gmail.com>
2025-12-27 02:36:24 +0600

    footer fix

--- footer.php
+++ footer.php
@@ -20,10 +20,10 @@
 
       <div class="box">
          <h3>contact info</h3>
-         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
-         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
-         <p> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </p>
-         <p> <i class="fas fa-map-marker-alt"></i> mumbai, india - 400104 </p>
+         <p> <i class="fas fa-phone"></i> 01923847193 </p>
+         <p> <i class="fas fa-phone"></i> 01637294719 </p>
+         <p> <i class="fas fa-envelope"></i> techstore@info.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Baridhara DOHS, Dhaka, Bangladesh </p>
       </div>
 
       <div class="box">
