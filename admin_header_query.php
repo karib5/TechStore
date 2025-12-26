1a019cd32dbdaadc3a8dc717e3903f42fb63e21b
sumshun-nahar-eity <esumshun@gmail.com>
2025-12-26 09:29:07 -0800

    admin header query error solved

--- admin_header_query.php
+++ admin_header_query.php
@@ -1,14 +1,16 @@
 <?php
-if (isset($message)) {
-   foreach ($message as $msg) {
-      echo '
-      <div class="message">
-         <span>'.$msg.'</span>
-         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
-      </div>
-      ';
-   }
+
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
 }
+
+if (!isset($_SESSION['admin_id'])) {
+    header('location:login.php');
+    exit;
+}
+
+$admin_id = $_SESSION['admin_id'];
 
 $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
 $select_profile->execute([$admin_id]);
